af7a9bc5e13040f7ab799943f13d3a14e19bf2f1
Stephen-Kimoi <moenisteve@gmail.com>
2023-10-04 18:12:46 +0300

    configuring the hardhat file for deploying on both testnets

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,21 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config(); 
+const { projectId, mnemonic } = require('./secrets.json'); // Replace with your Infura project ID and mnemonic
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    hardhat: {}, 
+    polygon_mumbai: {
+      url: process.env.ALCHEMY_API_KEY, 
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/",
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
+  solidity: {
+    version: '0.8.0',
+  },
 };
